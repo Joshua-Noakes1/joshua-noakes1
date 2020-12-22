4c53c9ebe99edb3f0a5d3e5fcc0f0c4076279514
Joshua-Noakes1 <joshua.noakes1@icloud.com>
2020-12-22 19:49:08 +0000

    added a suprise for xmax

--- index.js
+++ index.js
@@ -5,8 +5,36 @@ const MUSTACHE_MAIN_DIR = './main.mustache';
 /**
  * DATA is the object that contains all
  * the data to be provided to Mustache
- * Notice the "name" and "date" property.
+ * Notice the 'name' and 'date' property.
  */
+
+// NYE and XMSD emoji changes
+
+const date = new Date().toLocaleDateString('en-GB', {
+    day: 'numeric',
+})
+const month = new Date().toLocaleDateString('en-GB', {
+    month: 'numeric',
+})
+
+if (month == 12) {
+    if (date == 25) {
+        var dayemoji = '🎄';
+    } else if (date == 31) {
+        var dayemoji = '🎉';
+    } else {
+        var dayemoji = '👋';
+    }
+} else if (month == 1) {
+    if (date == 1) {
+        var dayemoji = '🎉';
+    } else {
+        var dayemoji = '👋';
+    }
+} else {
+    var dayemoji = '👋';
+}
+
 let DATA = {
     name: 'Joshua',
     date: new Date().toLocaleDateString('en-GB', {
@@ -20,6 +48,7 @@ let DATA = {
         minute: 'numeric',
         timeZone: 'Europe/London',
     }),
+    emoji: dayemoji,
 };
 /**
  * A - We open 'main.mustache'
