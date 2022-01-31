2d306e0709f0f30caf364f62fa18a60113376e82
Joshua Noakes <joshua.noakes1@icloud.com>
2022-01-31 19:16:40 +0000

    Disable saul gif

--- bin/currentGif.js
+++ bin/currentGif.js
@@ -7,7 +7,7 @@ async function currentGif() {
     switch (Math.round(Math.random() * 3)) {
         case 0:
         case 1:
-            var gifs = await readJSON(path.join(__dirname, 'gifs', 'saul.json'), true);
+            var gifs = await readJSON(path.join(__dirname, 'gifs', 'jinx.json'), true);
             break;
         case 2:
         case 3:
