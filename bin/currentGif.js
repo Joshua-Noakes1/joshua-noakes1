26b8af92107c3873e13d5582c6bfa8ceb59c65e0
Joshua Noakes <joshua.noakes1@icloud.com>
2022-02-01 00:07:23 +0000

    Revert "updated gif picker"
    This reverts commit eb71c4e5454359b5296996560a466a3856961779.

--- bin/currentGif.js
+++ bin/currentGif.js
@@ -4,8 +4,17 @@ const {
     path = require('path');
 
 async function currentGif() {
-    var listOfGifs = ["path.join(__dirname, 'gifs', 'jinx.json')"];
-    var gifs = await readJSON(listOfGifs[Math.floor(Math.random() * listOfGifs.length)], true);
+    switch (Math.round(Math.random() * 3)) {
+        case 0:
+        case 1:
+            var gifs = await readJSON(path.join(__dirname, 'gifs', 'jinx.json'), true);
+            break;
+        case 2:
+        case 3:
+            var gifs = await readJSON(path.join(__dirname, 'gifs', 'jinx.json'), true);
+            break;
+    }
+
     return gifs[Math.floor(Math.random() * gifs.length)];
 }
 
