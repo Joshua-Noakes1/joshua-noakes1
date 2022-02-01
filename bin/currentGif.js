b27d9e2b51824b8d88958ad35548b687c0e57091
Joshua Noakes <joshua.noakes1@icloud.com>
2022-02-01 00:15:54 +0000

    actual commit

--- bin/currentGif.js
+++ bin/currentGif.js
@@ -4,17 +4,8 @@ const {
     path = require('path');
 
 async function currentGif() {
-    switch (Math.round(Math.random() * 3)) {
-        case 0:
-        case 1:
-            var gifs = await readJSON(path.join(__dirname, 'gifs', 'jinx.json'), true);
-            break;
-        case 2:
-        case 3:
-            var gifs = await readJSON(path.join(__dirname, 'gifs', 'jinx.json'), true);
-            break;
-    }
-
+    var listOfGifs = [path.join(__dirname, 'gifs', 'jinx.json')];
+    var gifs = await readJSON(listOfGifs[Math.floor(Math.random() * listOfGifs.length)], true);
     return gifs[Math.floor(Math.random() * gifs.length)];
 }
 
