d6b88933caaedbbd53b85065e4c498bbe81a8445
Joshua Noakes <joshua.noakes1@icloud.com>
2022-02-23 14:35:00 +0000

    re-add saul gifs

--- bin/currentGif.js
+++ bin/currentGif.js
@@ -4,7 +4,7 @@ const {
     path = require('path');
 
 async function currentGif() {
-    var listOfGifs = [path.join(__dirname, 'gifs', 'jinx.json'), path.join(__dirname, 'gifs', 'junko.json')];
+    var listOfGifs = [path.join(__dirname, 'gifs', 'jinx.json'), path.join(__dirname, 'gifs', 'junko.json'), path.join(__dirname, 'gifs', 'saul.json')];
     var gifs = await readJSON(listOfGifs[Math.floor(Math.random() * listOfGifs.length)], true);
     return gifs[Math.floor(Math.random() * gifs.length)];
 }
